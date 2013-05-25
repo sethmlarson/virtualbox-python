9f0a6e11e11c4afba0f2616a30abaf5777ce66c6
Michael Dorman <mjdorma@gmail.com>
2013-05-25 16:41:10 +1000

    VirtualBox and Session now have __doc__ from parents

--- virtualbox/__init__.py
+++ virtualbox/__init__.py
@@ -6,6 +6,7 @@ manager = vboxapi.VirtualBoxManager(None, None)
 
 
 class VirtualBox(library.IVirtualBox):
+    __doc__ = library.IVirtualBox.__doc__
     def __init__(self, interface=None):
         if interface is None:
             self._i = manager.getVirtualBox()
@@ -14,6 +15,7 @@ class VirtualBox(library.IVirtualBox):
 
 
 class Session(library.ISession):
+    __doc__ = library.ISession.__doc__
     def __init__(self, interface=None):
         if interface is None:
             self._i = manager.platform.getSessionObject(None)
