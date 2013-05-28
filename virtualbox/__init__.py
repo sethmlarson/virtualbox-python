30afbc92d6132e5e820fbac4f50828dd6498fe70
Michael Dorman <mjdorma@gmail.com>
2013-05-28 17:32:58 +1000

    now easier to extend arbitary interfaces

--- virtualbox/__init__.py
+++ virtualbox/__init__.py
@@ -1,28 +1,8 @@
-import vboxapi
-from virtualbox import library
-
+from extends import library
 
 __doc__ = library.__doc__
 
-
-manager = vboxapi.VirtualBoxManager(None, None)
-
-
-class VirtualBox(library.IVirtualBox):
-    __doc__ = library.IVirtualBox.__doc__
-    def __init__(self, interface=None):
-        if interface is None:
-            self._i = manager.getVirtualBox()
-        else:
-            self._i = interface
-
-
-class Session(library.ISession):
-    __doc__ = library.ISession.__doc__
-    def __init__(self, interface=None):
-        if interface is None:
-            self._i = manager.platform.getSessionObject(None)
-        else:
-            self._i = interface
+VirtualBox = library.IVirtualBox
+Session = library.ISession
 
 
