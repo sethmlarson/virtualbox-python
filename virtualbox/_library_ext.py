8315eab7e94c9efa598eb2793010fdc43ba7fd89
Michael Dorman <mjdorma@gmail.com>
2013-06-27 18:56:44 +1000

    added context helpers to ISession and IGuestSession.  added a create_session function to IMachine to facilitate the creation of a locked ISession over an IMachine object

--- virtualbox/_library_ext.py
+++ virtualbox/_library_ext.py
@@ -18,14 +18,23 @@ manager = vboxapi.VirtualBoxManager(None, None)
 from virtualbox._keyboard_ext import IKeyboard
 
 
-# Configure IVirtualBox bootstrap to build from vboxapi getVirtualBox
-class IVirtualBox(library.IVirtualBox):
-    __doc__ = library.IVirtualBox.__doc__
-    def __init__(self, interface=None):
-        if interface is None:
-            self._i = manager.getVirtualBox()
-        else:
-            self._i = interface
+# Add context management to IGuestSession
+class IGuestSession(library.IGuestSession):
+    __doc__ = library.IGuestSession.__doc__
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_val, trace):
+        self.close()
+        
+
+# Define some default params for create session 
+class IGuest(library.IGuest):
+    __doc__ = library.IGuest.__doc__
+    def create_session(self, user, password, domain='', session_name='pyvbox'):
+        return super(IGuest, self).create_session(user, password, domain,
+                                                    session_name)
+    create_session.__doc__ = library.IGuest.create_session.__doc__
 
 
 # Configure ISession bootstrap to build from vboxapi getSessionObject
@@ -36,14 +45,49 @@ class ISession(library.ISession):
             self._i = manager.platform.getSessionObject(None)
         else:
             self._i = interface
+    
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_val, trace):
+        self.unlock_machine()
 
 
-# Fix a what seems to be a buggy definition for deleting machine config
+# Configure IVirtualBox bootstrap to build from vboxapi getVirtualBox
+class IVirtualBox(library.IVirtualBox):
+    __doc__ = library.IVirtualBox.__doc__
+    def __init__(self, interface=None):
+        if interface is None:
+            self._i = manager.getVirtualBox()
+        else:
+            self._i = interface
+
+
+# Extend and fix IMachine :) 
 class IMachine(library.IMachine):
     __doc__ = library.IMachine.__doc__
 
+    # Fix a what seems to be a buggy definition for deleting machine config
     # Testing showed that deleteConfig was just 'delete'
     _delete_config = 'delete'
+
+    # Add a helper to make locking and building a session simple
+    def create_session(self, lock_type=library.LockType.shared,
+                   session=None):
+        """Lock this machine
+        
+        Arguments:
+            lock_type - see IMachine.lock_machine for details
+            session - optionally define a session object to lock this machine 
+                      against.  If not defined, a new ISession object is 
+                      created to lock against
+        
+        return an ISession object
+        """ 
+        if session is None:
+            session = library.ISession()
+        self.lock_machine(session, lock_type)
+        return session
 
 
 # Helper function for IProgress to print out a current progress state
