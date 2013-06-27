0590e07b1b9b63f5d34374d22266de9043295736
Michael Dorman <mjdorma@gmail.com>
2013-06-27 20:09:10 +1000

    added execute into IGuestSession.  IGuest create_session now has a blocking option to wait until the guest service is available on the host.

--- virtualbox/_library_ext.py
+++ virtualbox/_library_ext.py
@@ -1,4 +1,5 @@
 import inspect
+import time
 
 from virtualbox import library
 
@@ -26,14 +27,87 @@ class IGuestSession(library.IGuestSession):
 
     def __exit__(self, exception_type, exception_val, trace):
         self.close()
-        
+
+    def execute(self, command, arguments=[], stdin="", environment=[],
+                 flags=[library.ProcessCreateFlag.wait_for_std_err,
+                        library.ProcessCreateFlag.wait_for_std_out,
+                        library.ProcessCreateFlag.ignore_orphaned_processes],
+                 priority=library.ProcessPriority.default,
+                 affinity=[], timeout_ms=0):
+        """Execute a command in the Guest
+
+            Arguments:
+                command - Command to execute.
+                arguments - List of arguments for the command
+                stdin - A buffer to write to the stdin of the command.
+                environment - See IGuestSession.create_process?
+                flags - List of ProcessCreateFlag objects.  
+                    Default value set to [wait_for_std_err, 
+                                          wait_for_stdout,
+                                          ignore_orphaned_processes]
+                timeout_ms - ms to wait for the process to complete.  
+                    If 0, wait for ever... 
+                priority - Set the ProcessPriority priority to be used for
+                    execution.
+                affinity - Process affinity to use for execution. 
+
+            Return IProcess, stdout, stderr 
+        """
+        def read_out(process, flags, stdout, stderr):
+            if library.ProcessCreateFlag.wait_for_std_err in flags:
+                e = process.read(2, 65000, 0)
+                stderr.append(e)
+            if library.ProcessCreateFlag.wait_for_std_out in flags:
+                o = process.read(1, 65000, 0)
+                stdout.append(o)
+
+        process = self.process_create_ex(command, arguments, environment,
+                            flags, timeout_ms, priority, affinity)
+        process.wait_for(int(library.ProcessWaitResult.start), 0)
+
+        # write stdin to the process 
+        if stdin:
+            index = 0
+            while index < len(stdin):
+                index += process.write(0, [library.ProcessInputFlag.none], 
+                                        stdin[index:], 0)
+            process.write(0, [library.ProcessInputFlag.end_of_file], 0)
+
+        # read the process output and wait for 
+        stdout = []
+        stderr = []
+        while process.status == library.ProcessStatus.started:
+            read_out(process, flags, stdout, stderr)
+            time.sleep(0.2)
+        # make sure we have read the remainder of the out
+        read_out(process, flags, stdout, stderr)
+        return process, "".join(stdout), "".join(stderr)
+
 
 # Define some default params for create session 
 class IGuest(library.IGuest):
     __doc__ = library.IGuest.__doc__
-    def create_session(self, user, password, domain='', session_name='pyvbox'):
-        return super(IGuest, self).create_session(user, password, domain,
+    def create_session(self, user, password, domain='', session_name='pyvbox',
+                        timeout_ms=0):
+        session = super(IGuest, self).create_session(user, password, domain,
                                                     session_name)
+        if timeout_ms != 0:
+            # There is probably a better way to to this?
+            if 'win' in self.os_type_id.lower():
+                test_file = 'C:\\autoexec.bat'
+            else:
+                test_file = '/bin/sh'
+            while True:
+                try:
+                    session.file_query_info(test_file)
+                except library.VBoxError as err:
+                    time.sleep(0.5)
+                    timeout_ms -= 500
+                    if timeout_ms <= 0:
+                        raise
+                    continue
+                break
+        return session
     create_session.__doc__ = library.IGuest.create_session.__doc__
 
 
