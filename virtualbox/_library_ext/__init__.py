9f562784c169199a0976d3128d1119e35856640f
mjdorma <mjdorma+github@gmail.com>
2013-07-05 17:09:31 +1000

    IVirtualBox and ISession now using virtualbox.Manager

--- virtualbox/_library_ext/__init__.py
+++ virtualbox/_library_ext/__init__.py
@@ -1,17 +1,13 @@
 import inspect
 
+import virtualbox
 from virtualbox import library
 
-import vboxapi
-manager = vboxapi.VirtualBoxManager(None, None)
-
 """
- This module is responsible for bootstrapping the COM interfaces into the 
- VirthalBox and Session class interfaces.
-
- It is also the place to import extension classes that fix up or improve on
- the default COM API behaviour and auto generated Python library file when
- interacting through an Interface to the Main library API.
+ This module is responsible for shimming out the auto generated libraries found
+ under librar.py.  The intension for the extension classes is to fix up or
+ improve on the default COM API behaviour and auto generated Python library
+ file when interacting through an Interface to the Main library API.
 """
 
 # Import extension modules
@@ -21,30 +17,36 @@ from virtualbox._library_ext.guest import IGuest
 from virtualbox._library_ext.machine import IMachine
 
 
+# Configure IVirtualBox bootstrap to build from vboxapi getVirtualBox
+class IVirtualBox(library.IVirtualBox):
+    __doc__ = library.IVirtualBox.__doc__
+    def __init__(self, interface=None, manager=None):
+        if interface is not None:
+            self._i = interface
+        elif manager is not None:
+            self._i = manager.get_virtualbox()
+        else:
+            manager = virtualbox.Manager()
+            self._i = manager.get_virtualbox()
+
+
 # Configure ISession bootstrap to build from vboxapi getSessionObject
 class ISession(library.ISession):
     __doc__ = library.ISession.__doc__
-    def __init__(self, interface=None):
-        if interface is None:
-            self._i = manager.platform.getSessionObject(None)
-        else:
+    def __init__(self, interface=None, manager=None):
+        if interface is not None:
             self._i = interface
+        elif manager is not None:
+            self._i = manager.get_session()
+        else:
+            manager = virtualbox.Manager()
+            self._i = manager.get_session()
     
     def __enter__(self):
         return self
 
     def __exit__(self, exception_type, exception_val, trace):
         self.unlock_machine()
-
-
-# Configure IVirtualBox bootstrap to build from vboxapi getVirtualBox
-class IVirtualBox(library.IVirtualBox):
-    __doc__ = library.IVirtualBox.__doc__
-    def __init__(self, interface=None):
-        if interface is None:
-            self._i = manager.getVirtualBox()
-        else:
-            self._i = interface
 
 
 # Replace original with extension
