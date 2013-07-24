f8965a0cbe0a71b34feeabc617a27a2f930ef8c2
mick <mjdorma@gmail.com>
2013-07-24 21:40:46 +1000

    fleshing out concept for machine pool

--- virtualbox/machinepool.py
+++ virtualbox/machinepool.py
@@ -1,36 +1,46 @@
 
-
 import virtualbox
-from virtualbox import manage
 from virtualbox import library
 
 
-def create_machine_pool(name, machine_or_name_or_id):
-    """create a pool of ready to execute liked clones from the Machine"""
-    pass
-
 
 class Pool(object):
-    def __init__(self, name):
-        self._name = name           
-        self._update_pool()
-
-    def _update_pool(self):
-        # load configuration for pool
-        pass
-    
-    def open(self, block=True):
-        """get a Machine resource from the pool"""
+    def __init__(self, root_machine, min_size=1):
+        self._root = root_machine
+        self._min_size = min_size
+        self._clone_name = '%s Pool' % self._root.name
+        self._machines = []
         vbox = virtualbox.VirtualBox()
         for vm in vbox.machines:
-            if not vm.name.startswtih(self._name):
+            if not vm.name.startswtih(self._root.name):
                 continue
-            if not '_pool_' in vm.name:
+            if not vm.name.startswith(self._clone_name):
                 continue
-            # check state, acquire resource
-#            if vm.machine_state ???
-#                pass
-                #
+            self._machines.append(vm)
+        self._adjust_pool()
 
-        return Machine(machine, session)
+    def _adjust_pool(self):
+        for i in range(self.min_size - len(self.machines)):
+            self._machines.append(self._root.clone(name=self._clone_name))
+
+    @property
+    def min_size(self):
+        return self._min_size
+
+    @settr.min_size
+    def min_size(self, min_size):
+        self._min_size = min_size
+        self._adjust_pool()
+
+    @property
+    def machines(self):
+        return self._machines
+
+    def __len__(self):
+        return len(self.machines) 
+    
+    
+
+
+
 
