d31c29d9ef1e59b5e1de37cbdc4687de7471bf5d
mick <mjdorma@gmail.com>
2013-07-24 23:30:39 +1000

    more thought... not there yet

--- virtualbox/machinepool.py
+++ virtualbox/machinepool.py
@@ -4,32 +4,25 @@ from virtualbox import library
 
 
 
-class Pool(object):
-    def __init__(self, root_machine, min_size=1):
+class ClonePool(object):
+    def __init__(self, root_machine, size=1):
         self._root = root_machine
-        self._min_size = min_size
+        self._size = size
         self._clone_name = '%s Pool' % self._root.name
         self._machines = []
         vbox = virtualbox.VirtualBox()
         for vm in vbox.machines:
-            if not vm.name.startswtih(self._root.name):
-                continue
             if not vm.name.startswith(self._clone_name):
                 continue
             self._machines.append(vm)
-        self._adjust_pool()
-
-    def _adjust_pool(self):
-        for i in range(self.min_size - len(self.machines)):
-            self._machines.append(self._root.clone(name=self._clone_name))
 
     @property
-    def min_size(self):
-        return self._min_size
+    def size(self):
+        return self._size
 
-    @settr.min_size
-    def min_size(self, min_size):
-        self._min_size = min_size
+    @settr.size
+    def size(self, size):
+        self._size = size
         self._adjust_pool()
 
     @property
@@ -38,9 +31,9 @@ class Pool(object):
 
     def __len__(self):
         return len(self.machines) 
-    
-    
 
 
+
+pool = Pool('cuckoo1', 10)
 
 
