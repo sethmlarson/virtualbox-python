f80525e95be9251c9d1b4a76b0464621f68c1f0b
mick <mjdorma@gmail.com>
2013-07-25 16:41:06 +1000

    defined IMouse with on guest mouse event callback setter

---
+++ virtualbox/library_ext/mouse.py
@@ -0,0 +1,23 @@
+from virtualbox import library
+
+
+"""
+Add helper code to the default IMouse class.
+"""
+
+
+class IMouse(library.IMouse):
+    __doc__ = library.IMouse.__doc__
+
+    def set_on_guest_mouse(self, callback):
+        """Set the callback function to consume on guest mouse events.
+        
+        Callback receives a IGuestMouseEvent object.
+
+        Example:
+            def callback(event):
+                print(("%s %s %s" % (event.x, event.y, event.z))     
+        """
+        return self.event_source.register_callback(callback, 
+                            library.VBoxEventType.on_guest_mouse)
+
