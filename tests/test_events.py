dfea37cd44c942cbd6173e172aa637fefcc0a0ce
mick <mjdorma@gmail.com>
2013-09-17 09:26:09 +1000

    confirm events were added

---
+++ tests/test_events.py
@@ -0,0 +1,19 @@
+import unittest 
+import time
+
+import virtualbox
+
+class TestEvents(unittest.TestCase):
+    
+    def extra_data_callback(self, event):
+        self.called = True
+        print("Event key=%s value=%s" % (event.key, event.value))
+
+    def test_extra_data_changed(self):
+        vbox = virtualbox.VirtualBox()
+        vbox.register_on_extra_data_changed(self.extra_data_callback)
+        # Cause a change event
+        vbox.set_extra_data('test', 'data')
+        vbox.set_extra_data('test', 'dataa')
+        self.assertTrue(self.called)
+
