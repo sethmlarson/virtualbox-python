81d259c4f4e58fb1699e0d74cc7a0e62a6b1d824
mick <mjdorma@gmail.com>
2013-10-09 19:53:18 +1100

    update IMachinen to be pep'ish compatible.

--- virtualbox/library_ext/machine.py
+++ virtualbox/library_ext/machine.py
@@ -192,25 +192,14 @@ class IMachine(library.IMachine):
     # BUG: xidl describes this function as exportTo.  The interface seems
     #      to export plain "export" instead... 
     def export_to(self, appliance, location):
-        """Exports the machine to an OVF appliance. See <link to="IAppliance"/> for the
-        steps required to export VirtualBox machines to OVF.
-
-        in appliance of type IAppliance
-            Appliance to export this machine to.
-
-        in location of type str
-            The target location.
-
-        return description of type IVirtualSystemDescription
-            VirtualSystemDescription object which is created for this machine.
-
-        """
         if not isinstance(appliance, IAppliance):
-            raise TypeError("appliance can only be an instance of type IAppliance")
-        if type(location) not in [str, unicode]:
-            raise TypeError("value is not a str or unicode")
+            msg = "appliance can only be an instance of type IAppliance"
+            raise TypeError(msg)
+        if not isinstance(location, basestring):
+            raise TypeError("value is not an instance of basestring")
         description = self._call("export",
                      in_p=[appliance, location])
         description = IVirtualSystemDescription(description)
         return description
+    export_to.__doc__ = library.IMachine.export_to.__doc__
 
