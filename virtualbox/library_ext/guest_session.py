7428f7d87d33ab1531f94753516ad4a56780a612
mjdorma <mjdorma@gmail.com>
2014-02-17 14:55:22 +1100

    Add helper to predefine remove recursive flag.  Add copy_to and copy_from helpers which aid the copying of single files

--- virtualbox/library_ext/guest_session.py
+++ virtualbox/library_ext/guest_session.py
@@ -75,3 +75,38 @@ class IGuestSession(library.IGuestSession):
         """Super-mkdir: create a leaf directory and all intermediate ones."""
         self.directory_create(path, mode, [library.DirectoryCreateFlag.parents])
 
+    # Simplify calling directory_remove_recursive.  Set default flags to
+    # content_and_dir if they have not yet been set.
+    def directory_remove_recursive(self, path, flags=None):
+        if flags is None:
+            flags = [library.DirectoryRemoveRecFlag.content_and_dir]
+        super(IGuestSession, self).directory_remove_recursive(path, flags)
+    directory_remove_recursive.__doc__ = \
+            library.IGuestSession.directory_remove_recursive.__doc__
+
+    def copy_to(self, host_path, guest_path):
+        "Copy a single file to the vm. Wraps copy_to_vm."
+        if not os.path.exists(host_path):
+            raise OSError("Failed to find %s on host" % host_path)
+        p = self.copy_to_vm(host_path, guest_path, [])
+        p.wait_for_completion()
+        return p
+
+    def copy_from(self, guest_path, host_path):
+        "Copy a single file from the vm. Wraps copy_from_vm."
+        # Dodgy exists check...
+        for x in range(10):
+            try:
+                self.file_exists(guest_path)
+                break
+            except:
+                time.sleep(0.1)
+        else:
+            raise OSError("Failed to find %s on guest" % guest_path)    
+        p = self.copy_from(guest_path, host_path, [])
+        p.wait_for_completion()
+        return p
+
+
+
+    
