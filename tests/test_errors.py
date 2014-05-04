1caf1600fed6c6bc571e0475b8cdfbed34122814
mjdorma <mjdorma@gmail.com>
2014-05-04 21:16:40 +1000

    Error raises test

---
+++ tests/test_errors.py
@@ -0,0 +1,18 @@
+import unittest
+
+import virtualbox
+from virtualbox.library import VBoxError
+
+class TestErrors(unittest.TestCase):
+
+    def test_raises(self):
+        vbox = virtualbox.VirtualBox()
+        try:
+            vbox.find_machine('blah blah X')
+        except VBoxError as exc:
+            pass
+        else:
+            self.fail("VBoxError not raised")
+
+
+
