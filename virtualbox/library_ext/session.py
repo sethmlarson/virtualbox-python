e181205fe3e931f61444e67e50bd60f760b36008
Seth Michael Larson <sethmichaellarson@protonmail.com>
2017-05-15 10:02:08 -0500

    Fix linting issues in virtualbox/library_ext/progress.py and ../session.py

--- virtualbox/library_ext/session.py
+++ virtualbox/library_ext/session.py
@@ -1,13 +1,14 @@
-import virtualbox
-from virtualbox import library
-
 """
 Add helper code to the default ISession class.
 """
+import virtualbox
+from virtualbox import library
+
 
 # Configure ISession bootstrap to build from vboxapi getSessionObject
 class ISession(library.ISession):
     __doc__ = library.ISession.__doc__
+
     def __init__(self, interface=None, manager=None):
         if interface is not None:
             super(ISession, self).__init__(interface)
@@ -20,5 +21,5 @@ class ISession(library.ISession):
     def __enter__(self):
         return self
 
-    def __exit__(self, exception_type, exception_val, trace):
+    def __exit__(self, *_):
         self.unlock_machine()
