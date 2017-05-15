c58e94f326f9659737a6b67614055c97f998f179
Seth Michael Larson <sethmichaellarson@protonmail.com>
2017-05-15 11:24:11 -0500

    Fix linting issues in virtualbox/library_ext/__init__.py

--- virtualbox/library_ext/__init__.py
+++ virtualbox/library_ext/__init__.py
@@ -1,8 +1,3 @@
-from __future__ import absolute_import
-import inspect
-
-from virtualbox import library
-
 """
  This module is responsible for shimming out the auto generated libraries found
  under library.py.  The intension for the extension classes is to fix up or
@@ -10,23 +5,28 @@ from virtualbox import library
  file when interacting through an Interface to the Main library API.
 """
 
+from __future__ import absolute_import
+import inspect
+
+from virtualbox import library
+
 
 # Import extension modules
-from .vbox import IVirtualBox
-from .session import ISession
-from .keyboard import IKeyboard
-from .guest_session import IGuestSession
-from .guest import IGuest
-from .host import IHost
-from .machine import IMachine
-from .progress import IProgress
-from .console import IConsole
-from .event_source import IEventSource
-from .mouse import IMouse
-from .process import IProcess
-from .guest_process import IGuestProcess
-from .appliance import IAppliance
-from .virtual_system_description import IVirtualSystemDescription
+from .vbox import IVirtualBox  # noqa: F401
+from .session import ISession  # noqa: F401
+from .keyboard import IKeyboard  # noqa: F401
+from .guest_session import IGuestSession  # noqa: F401
+from .guest import IGuest  # noqa: F401
+from .host import IHost  # noqa: F401
+from .machine import IMachine  # noqa: F401
+from .progress import IProgress  # noqa: F401
+from .console import IConsole  # noqa: F401
+from .event_source import IEventSource  # noqa: F401
+from .mouse import IMouse  # noqa: F401
+from .process import IProcess  # noqa: F401
+from .guest_process import IGuestProcess  # noqa: F401
+from .appliance import IAppliance  # noqa: F401
+from .virtual_system_description import IVirtualSystemDescription  # noqa: F401
 
 
 # Replace original with extension
