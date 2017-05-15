2e403a17a8694cf6846631b03c72c5e7ff9f4284
Seth Michael Larson <sethmichaellarson@protonmail.com>
2017-05-15 10:55:13 -0500

    Fix linting issues in virtualbox/library_ext/keyboard.py

--- virtualbox/library_ext/guest_process.py
+++ virtualbox/library_ext/guest_process.py
@@ -5,6 +5,7 @@ Add helper code to the default IGuestProcess class.
 from virtualbox import library
 from virtualbox.library_ext import process
 
+
 # Because there is a bug in IProcess, which is the super class of
 # IGuestProcess, we'll inherit from our fixed IProcess to ensure
 # that IGuestPRocess behaves correctly.
